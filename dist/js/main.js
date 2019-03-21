f815e1803789fdda0f145ab80974bbf264dace1f
samashtar <sam.ashtar@flatironschool.com>
2019-03-21 04:28:41 -0400

    type n speak works

--- dist/js/main.js
+++ dist/js/main.js
@@ -32,3 +32,63 @@ getVoices();
 if (synth.onvoiceschanged !== undefined) {
   synth.onvoiceschanged = getVoices;
 }
+
+//Speak
+
+const speak = () => {
+  // check if speaking
+  if (synth.speaking) {
+    console.error("Already speaking...");
+    return;
+  }
+
+  if (textInput.value !== "") {
+    //this is what is spoken
+    const speakText = new SpeechSynthesisUtterance(textInput.value);
+
+    //stop speaking
+    speakText.onend = e => {
+      console.log("done speaking...");
+    };
+
+    // error
+    speakText.onerror = e => {
+      console.error("something went wrong");
+    };
+    // selected voice
+    const selectedVoice = voiceSelect.selectedOptions[0].getAttribute(
+      "data-name"
+    );
+
+    voices.forEach(voice => {
+      if (voice.name === selectedVoice) {
+        speakText.voice = voice;
+      }
+    });
+
+    // set pitch and rate
+    speakText.rate = rate.value;
+    speakText.pitch = pitch.value;
+
+    //Speak
+    synth.speak(speakText);
+  }
+};
+
+// Event listeners
+
+//Text Form Submit
+textForm.addEventListener("submit", e => {
+  e.preventDefault();
+  speak();
+  textInput.blur();
+});
+
+// rate value change
+rate.addEventListener("change", e => (rateValue.textContent = rate.value));
+
+//pitch value change
+pitch.addEventListener("change", e => (pitchValue.textContent = pitch.value));
+
+// voice select change
+voiceSelect.addEventListener("change", e => speak());
